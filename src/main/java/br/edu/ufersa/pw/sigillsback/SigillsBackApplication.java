97311f19f081ac1885e016189f6dab89777e5e8d
Fernando Silva <fernnlcs@gmail.com>
2022-06-08 20:07:19 -0300

    Feat: User Entity

--- src/main/java/br/edu/ufersa/pw/sigillsback/SigillsBackApplication.java
+++ src/main/java/br/edu/ufersa/pw/sigillsback/SigillsBackApplication.java
@@ -1,10 +1,14 @@
 package br.edu.ufersa.pw.sigillsback;
 
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SigillsBackApplication {
+
+	// private static final Logger logger = LoggerFactory.getLogger(SigillsBackApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigillsBackApplication.class, args);

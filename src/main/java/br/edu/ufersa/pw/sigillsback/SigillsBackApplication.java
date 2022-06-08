3f12a327f64c3cd7f84e2b70c73b6b069110b5ae
Fernando Silva <fernnlcs@gmail.com>
2022-06-07 22:10:30 -0300

    First commit

---
+++ src/main/java/br/edu/ufersa/pw/sigillsback/SigillsBackApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pw.sigillsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigillsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigillsBackApplication.class, args);
+	}
+
+}
